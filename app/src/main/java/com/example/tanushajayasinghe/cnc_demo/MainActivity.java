7d70a4b318241a39a65ff351e8b7874a59a9f1eb
Tanusha Jayasinghe <djtanushajayasinghe@gmail.com>
2017-12-24 15:54:54 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/tanushajayasinghe/cnc_demo/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.tanushajayasinghe.cnc_demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallbackExtended;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    MQTTHelper mqttHelper;
+    EditText editText;
+    Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startMqtt();
+        editText = findViewById(R.id.z_field);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mqttHelper.sendMessage(editText.getText().toString());
+            }
+        });
+
+    }
+
+    private void startMqtt(){
+        mqttHelper = new MQTTHelper(getApplicationContext());
+        mqttHelper.setCallback(new MqttCallbackExtended() {
+            @Override
+            public void connectComplete(boolean b, String s) {
+                Toast.makeText(getApplicationContext(), "Connected to the server", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void connectionLost(Throwable throwable) {
+
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                Log.w("Debug",mqttMessage.toString());
+//                dataReceived.setText(mqttMessage.toString());
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+}
